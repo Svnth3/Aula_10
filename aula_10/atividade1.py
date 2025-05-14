c4e11420102987f795f9a1798d36d143b6a4fc2a
Svnth3 <marinsfraga@gmail.com>
2025-05-13 21:29:16 -0300

    Ultimo modelo

--- aula_10/atividade1.py
+++ aula_10/atividade1.py
@@ -29,19 +29,19 @@ print('multiplicar [*], adição [+], divisão [/], subtração [-]')
 operacao = input("Qual operação: ")
 print(f'Os numeros gerados foram {numero1} e {numero2}')
 
-soma = adicionar(numero1, numero2)
-sub = subtrair(numero1, numero2)
-div = dividir(numero1, numero2)
-mult = multiplicar(numero1, numero2)
 
 match operacao:
     case "+":
+        soma = adicionar(numero1, numero2)
         print(f'O resultado da soma é {soma}.')
     case "-":
+        sub = subtrair(numero1, numero2)
         print(f'O resultado da subtação é {sub}.')
     case "/":
+        div = dividir(numero1, numero2)
         print(f'O resultado da divisão é {div}.')
     case "*":
+        mult = multiplicar(numero1, numero2)
         print(f'O resultado da multiplicação é {mult}.')
     case _:
         print("Operador inválido")
